130aeddfb1a30601ea74f38a67cecfb31e2f572b
Nick Budak <thatbudakguy@gmail.com>
2018-02-26 14:06:55 -0800

    Update bs-config.js
    require wpconf to get package name

--- bs-config.js
+++ bs-config.js
@@ -1,5 +1,6 @@
 const proxy = process.env.PROXY || 'http://alliance-primo-sb.hosted.exlibrisgroup.com'
 const vid = process.env.VID || 'ALLIANCE'
+const wpconf  = require('./webpack.config.js')
 const fname = Object.keys(wpconf.entry)[0]
 
 // 'http://alliance-primo-sb.hosted.exlibrisgroup.com'
